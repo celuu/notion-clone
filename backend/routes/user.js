098bae1492960cad8c6bb70ffc2eafac210cafaa
celuu <christine.e.luu@gmail.com>
2023-05-08 16:37:31 -0700

    Finish signup function

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 
+//controller functions
+const {loginUser, signupUser} = require('../controllers/userController')
 
+//login route
+router.post('/login', loginUser)
+
+//signup router
+router.post("/signup", signupUser);
 
 
 
